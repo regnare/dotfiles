5bb763b3458e8245a4918d9372ead33e94384409
Ben Olson <regnare@gmail.com>
2020-05-05 05:38:26 -0400

    add package check for stow

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+dpkg-query -W -f '${Status}' stow 2>/dev/null| grep -q "ok installed" \
+  || (echo "Installing stow..."; sudo apt install stow)
+
 for d in $(find . -maxdepth 1 -type d | sed 's/.\///; /^\..*$/d'); do
   stow -t ~ $d
 done
