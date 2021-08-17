70d48b9c90d96c291b72819ee712fe7588d90a4a
Ben Olson <regnare@gmail.com>
2021-08-17 17:55:16 -0400

    Update setup.sh
    Add fzf to setup auto install.

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 dpkg-query -W -f '${Status}' stow 2>/dev/null| grep -q "ok installed" \
-  || (echo "Installing stow..."; sudo apt install stow)
+  || (echo "Installing stow..."; sudo apt -y install stow)
+dpkg-query -W -f '${Status}' fzf 2>/dev/null| grep -q "ok installed" \
+  || (echo "Installing fzf..."; sudo apt -y install fzf)
 
 for d in $(find . -maxdepth 1 -type d | sed 's/.\///; /^\..*$/d'); do
   stow -t ~ $d
